bb32de23869e8e268b2cf93c6e0280079017ff94
Alexander Barry <Alexander.Barry@keyin.com>
2025-06-20 12:50:52 -0230

    Created Venue class for Client.

---
+++ src/main/java/com/keyin/domain/Venue.java
@@ -0,0 +1,59 @@
+package com.keyin.domain;
+
+import java.util.Objects;
+
+public class Venue {
+    private long id;
+    private String name;
+    private String address;
+    private int capacity;
+
+    public Venue() {
+    }
+
+    public Venue(String name, String address, int capacity) {
+        this.name = name;
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public Venue(long id, String name, String address, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+}
